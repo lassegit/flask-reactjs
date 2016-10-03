f97a3bf9d413cb68383b647d50521200b612cb7a
Lasse <lassetange@gmail.com>
2016-10-03 19:10:45 +0200

    Ups add action index.js file

---
+++ app/static/src/actions/index.js
@@ -0,0 +1,9 @@
+/* Exports all the actions from a single point.
+
+Allows to import actions like so:
+
+import {action1, action2} from '../actions/'
+*/
+/* Populated by react-webpack-redux:action */
+const actions = {};
+module.exports = actions;
