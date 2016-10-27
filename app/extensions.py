58cb989913f74d9f75e4aaf76e8c5389ed510653
Lasse <lassetange@gmail.com>
2016-10-27 11:43:06 +0200

    Add correct login url_for

--- app/extensions.py
+++ app/extensions.py
@@ -21,7 +21,7 @@ mailgun = Mailgun()
 babel = Babel()
 
 login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
+login_manager.login_view = 'auth.signin'
 login_manager.login_message_category = 'warning'
 
 @login_manager.user_loader
